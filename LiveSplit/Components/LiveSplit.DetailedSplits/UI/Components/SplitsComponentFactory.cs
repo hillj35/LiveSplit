eaa88732ea359e7f481bed569136c94104933add
Jordan Hill <jordan.hill@comcast.net>
2017-06-21 16:37:12 -0700

    Added Component Named "DetailedSplits"
    Added a component named "DetailedSplits" that right now is just a copy of normal splits.

---
+++ LiveSplit/Components/LiveSplit.DetailedSplits/UI/Components/SplitsComponentFactory.cs
@@ -0,0 +1,24 @@
+﻿using LiveSplit.Model;
+using System;
+
+namespace LiveSplit.UI.Components
+{
+    public class SplitsComponentFactory : IComponentFactory
+    {
+        public string ComponentName => "Detailed Splits";
+
+        public string Description => "Displays splits as a normal splits component, but displays the current split in more detail.";
+
+        public ComponentCategory Category => ComponentCategory.List;
+
+        public IComponent Create(LiveSplitState state) => new SplitsComponent(state);
+
+        public string UpdateName => ComponentName;
+
+        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.DetailedSplits.xml";
+
+        public string UpdateURL => "http://livesplit.org/update/";
+
+        public Version Version => Version.Parse("1.7.3");
+    }
+}
